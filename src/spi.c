0353f3c481f73dc03597e8fcd1b9d481c5607a73
Tijana Stankovic <153671519+tijanastankovic@users.noreply.github.com>
2026-05-09 15:40:08 +0200

    Implement SPI driver initialization, transmit, receive and full-duplex transfer functions

--- src/spi.c
+++ src/spi.c
@@ -1,1 +1,43 @@
+#include "spi.h"
 
+void spi_init(void)
+{
+  SPI_CR |= (SPI_ENABLE | SPI_MASTER_MODE);
+}
+
+void spi_send_byte(uint8_t data) 
+{
+  while (!(SPI_SR & SPI_TX_READY))
+    {
+      // wait
+    }
+  
+  SPI_DR = data;
+}
+
+uint8_t spi_receive_byte(void)
+{
+  while (!(SPI_SR & SPI_RX_READY))
+    {
+      // wait
+    }
+
+  return (uint8_t)SPI_DR;
+}
+
+uint8_t spi_transfer_byte(uint8_t data)
+{
+  while (!(SPI_SR & SPI_TX_READY))
+    {
+      // wait
+    }
+  
+  SPI_DR = data;
+
+  while (!(SPI_SR & SPI_RX_READY)) 
+    {
+      // wait
+    }
+
+  return (uint8_t)SPI_DR;
+}
